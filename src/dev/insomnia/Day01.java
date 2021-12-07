567ddb8b1381a0214d31df0d90050eaa607471be
Adrian <adrian.hoeppener@alumni.fh-aachen.de>
2021-12-07 04:35:07 +0100

    Advent Code Start one

--- src/dev/insomnia/Day01.java
+++ src/dev/insomnia/Day01.java
@@ -1,6 +1,6 @@
 package dev.insomnia;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 public class Day01 extends AOCPuzzle {
@@ -13,8 +13,16 @@ public class Day01 extends AOCPuzzle {
     @Override
     void solve(List<String> input)
     {
-        List<Integer>detphs = convertToInts(input);
-        System.out.println("Test");
+        List<Integer>depths = convertToInts(input);
+        int changes =0;
+        for (int i= 1; i < depths.size(); i++)
+        {
+            if(depths.get(i - 1) < depths.get(i))
+            {
+                changes++;
+            }
+        }
+        lap(changes);
     }
 
 
