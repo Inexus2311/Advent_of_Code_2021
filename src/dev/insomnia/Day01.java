f6da061f3eb8b37ffacbc960aebfb0f2b1dc9d47
Adrian <adrian.hoeppener@alumni.fh-aachen.de>
2021-12-07 02:52:41 +0100

    Advent Code Kickoff Starter

---
+++ src/dev/insomnia/Day01.java
@@ -0,0 +1,23 @@
+package dev.insomnia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day01 extends AOCPuzzle {
+
+    public Day01()
+    {
+        super("1");
+    }
+
+    @Override
+    void solve(List<String> input)
+    {
+        List<Integer>detphs = convertToInts(input);
+        System.out.println("Test");
+    }
+
+
+
+}
+
