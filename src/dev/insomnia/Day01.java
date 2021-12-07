ffbe7b83a5f37d9b84b97f2aa909fc07270a32fc
Adrian <adrian.hoeppener@alumni.fh-aachen.de>
2021-12-07 18:26:00 +0100

    Advent Code Day 1 Complete

--- src/dev/insomnia/Day01.java
+++ src/dev/insomnia/Day01.java
@@ -1,7 +1,9 @@
 package dev.insomnia;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
+
 import java.util.List;
+
 
 public class Day01 extends AOCPuzzle {
 
@@ -10,22 +12,69 @@ public class Day01 extends AOCPuzzle {
         super("1");
     }
 
-    @Override
-    void solve(List<String> input)
+    void println_measurement(boolean change)
     {
-        List<Integer>depths = convertToInts(input);
-        int changes =0;
-        for (int i= 1; i < depths.size(); i++)
+        if(change)
         {
-            if(depths.get(i - 1) < depths.get(i))
-            {
+            System.out.println(" (increased)");
+        }
+        else{
+            System.out.println(" (decreased)");
+        }
+    }
+    @Override
+    void solve(List<String> input) {
+        List<Integer> depths = convertToInts(input);
+        int changes = 0;
+        for (int i = 1; i < depths.size(); i++) {
+            if (depths.get(i - 1) < depths.get(i)) {
                 changes++;
             }
         }
         lap(changes);
+
+        // Group numbers in a three-measurement input
+
+        List<Integer> windows = new ArrayList<>();
+        for(int i = 2; i < depths.size(); i++) {
+
+            windows.add(depths.get(i) + depths.get(i -1) + depths.get(i -2 ));
+        }
+
+        changes = 0;
+        for (int i = 1; i < windows.size(); i++) {
+            if (windows.get(i - 1) < windows.get(i)) {
+                changes++;
+            }
+        }
+        lap(changes);
+
+        boolean flag = true;
+
+        for (int i = 0; i < windows.size(); i++) {
+
+            if (flag)
+            {
+                flag=false;
+                System.out.print("Startnumber: ");
+                System.out.print(windows.get(i));
+                System.out.println(" (N/a no previous measurement)");
+            }
+            else{
+                if (windows.get(i - 1) < windows.get(i)) {
+
+                    System.out.print(windows.get(i));
+                    println_measurement(true);
+                }
+                else
+                {
+                    System.out.print(windows.get(i));
+                    println_measurement(false);
+                }
+            }
+
+
+        }
     }
-
-
-
 }
 
