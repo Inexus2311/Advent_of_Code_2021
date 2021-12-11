2d04fbfedbaf2516c6b0a11e0f1468b1b200c01d
Adrian Höppener <adrian.hoeppener@alumni.fh-aachen.de>
2021-12-11 20:26:59 +0100

    Day 2 Solving Task 1  & Task2 Putting Task 1,2 into Functions

--- src/dev/insomnia/Day02.java
+++ src/dev/insomnia/Day02.java
@@ -9,25 +9,56 @@ public class Day02 extends AOCPuzzle {
         super("2");
     }
 
-
-
-    @Override
-    void solve(List<String> input) {
-
+    void Task1(List<String> input)
+    {
         long horizontal = 0;
         long depth = 0;
 
         for(String s :input)
         {
-            String[] instructionparts = s.split(" ");
-            switch (instructionparts[0]) {
-                case "up" -> depth -= Integer.parseInt(instructionparts[1]);
-                case "forward" -> horizontal += Integer.parseInt(instructionparts[1]);
-                case "down" -> depth += Integer.parseInt(instructionparts[1]);
-
+            String[] list = s.split(" ");
+            switch (list[0]) {
+                case "up" -> depth -= Integer.parseInt(list[1]);
+                case "forward" -> horizontal += Integer.parseInt(list[1]);
+                case "down" -> depth += Integer.parseInt(list[1]);
             }
-
         }
         lap(horizontal*depth);
+    }
+
+
+    void Task2(List<String>input)
+    {
+
+        // Variables
+        long horizontal = 0;
+        long depth = 0;
+        long aim = 0;
+
+        for(String s: input)
+        {
+
+            String [] list = s.split(" ");
+
+            switch(list[0])
+            {
+                case "up" -> aim -= Integer.parseInt(list[1]);
+                case "forward" -> {
+                    horizontal += Integer.parseInt(list[1]);
+                    depth += (aim * Integer.parseInt(list[1]));
+                }
+                case "down" -> aim += Integer.parseInt(list[1]);
+            }
+        }
+        System.out.print("depth: ");lap(depth*horizontal);
+        System.out.print("horizontal: " + horizontal);
+        System.out.println(" ");
+    }
+    @Override
+    void solve(List<String> input) {
+
+        Task1(input);
+        Task2(input);
+
     }
 }
